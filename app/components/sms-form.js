5ddb002d0d6b44210639613cd95517de57731346
codemagnus <brenthmiras2@gmail.com>
2018-05-17 00:12:16 +0800

    Add basic styles

--- app/components/sms-form.js
+++ app/components/sms-form.js
@@ -22,36 +22,46 @@ export default class SMSForm extends Component {
         e.preventDefault();
 
         const {phone, message} = this.state;
-        
+
         /* TODO:  do something with phone and message */
     }
 
     handleChange(e) {
         const target = e.target;
-        const name   = target.name;
-        const value  = target.value;
+        const name = target.name;
+        const value = target.value;
 
-        this.setState({
-            [name] : value
-        });
+        this.setState({[name]: value});
     }
 
     render() {
         const {phone, message} = this.state;
 
         return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Phone number
-                    </label>
-                    <input name="phone" value={phone} onChange={this.handleChange}/>
-                    <label>
-                        Message
-                    </label>
-                    <textarea name="message" value={message} onChange={this.handleChange}></textarea>
-                    <button>Send</button>
-                </form>
+            <div className="jumbotron d-flex align-items-center">
+                <div className="container text-center">
+                    <div className="card mx-auto col-md-4">
+                        <div className="card-body">
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="form-group">
+                                    <label>
+                                        Phone number
+                                    </label>
+                                    <input className="form-control" name="phone" value={phone} onChange={this.handleChange}/>
+                                </div>
+
+                                <div className="form-group">
+                                    <label>
+                                        Message
+                                    </label>
+                                    <textarea className="form-control" name="message" value={message} onChange={this.handleChange}></textarea>
+                                </div>
+
+                                <button className="btn btn-primary btn-block">Send</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
