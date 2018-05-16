a97131fc9bfb65f23f7db53ce28096c3976168ae
codemagnus <brenthmiras2@gmail.com>
2018-05-16 23:45:02 +0800

    Handle text message value change

--- app/components/sms-form.js
+++ app/components/sms-form.js
@@ -1,17 +1,50 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 export default class SMSForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            message: ''
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        
+        const {message} = this.state;
+
+        /* TODO:  */
+    }
+
+    handleChange(e) {
+        const value = e.target.value;
+        
+        this.setState({
+            message: value
+        });
+    }
+
     render() {
+        const {message} = this.state;
+
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <label>
                         Message
                     </label>
-                    <textarea></textarea>
+                    <textarea
+                        value={message}
+                        onChange={this.handleChange}>
+                    </textarea>
                     <button>Send</button>
                 </form>
             </div>
-        )
+        );
     }
 }
